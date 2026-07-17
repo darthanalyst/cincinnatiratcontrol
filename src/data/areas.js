9489e902c10ad2aa64411ed9129b52f03c8f40f4
darthanalyst <78324713+darthanalyst@users.noreply.github.com>
2026-07-17 20:31:03 +0800

    Add services.js and areas.js combiners

---
+++ src/data/areas.js
@@ -0,0 +1,28 @@
+// Combined areas data (split into per-entry modules for reliable transfer).
+import _0 from "./areas/rat-control-services-in-norwood.js";
+import _1 from "./areas/rat-control-services-in-blue-ash.js";
+import _2 from "./areas/rat-control-services-in-mason.js";
+import _3 from "./areas/rat-control-services-in-west-chester.js";
+import _4 from "./areas/rat-control-services-in-fairfield.js";
+import _5 from "./areas/rat-control-services-in-hamilton.js";
+import _6 from "./areas/rat-control-services-in-loveland.js";
+import _7 from "./areas/rat-control-services-in-anderson-township.js";
+import _8 from "./areas/rat-control-services-in-delhi-township.js";
+import _9 from "./areas/rat-control-services-in-covington.js";
+import _10 from "./areas/rat-control-services-in-newport.js";
+import _11 from "./areas/rat-control-services-in-florence.js";
+
+export const areas = {
+  "rat-control-services-in-norwood": _0,
+  "rat-control-services-in-blue-ash": _1,
+  "rat-control-services-in-mason": _2,
+  "rat-control-services-in-west-chester": _3,
+  "rat-control-services-in-fairfield": _4,
+  "rat-control-services-in-hamilton": _5,
+  "rat-control-services-in-loveland": _6,
+  "rat-control-services-in-anderson-township": _7,
+  "rat-control-services-in-delhi-township": _8,
+  "rat-control-services-in-covington": _9,
+  "rat-control-services-in-newport": _10,
+  "rat-control-services-in-florence": _11,
+};
---
+++ src/data/services.js
@@ -0,0 +1,22 @@
+// Combined services data (split into per-entry modules for reliable transfer).
+import _0 from "./services/rat-control-in-cincinnati.js";
+import _1 from "./services/rat-exterminator-in-cincinnati.js";
+import _2 from "./services/rat-removal-in-cincinnati.js";
+import _3 from "./services/rat-inspection-in-cincinnati.js";
+import _4 from "./services/rodent-exclusion-in-cincinnati.js";
+import _5 from "./services/attic-rat-cleanup-in-cincinnati.js";
+import _6 from "./services/dead-rat-removal-in-cincinnati.js";
+import _7 from "./services/commercial-rat-control-in-cincinnati.js";
+import _8 from "./services/mouse-control-in-cincinnati.js";
+
+export const services = {
+  "rat-control-in-cincinnati": _0,
+  "rat-exterminator-in-cincinnati": _1,
+  "rat-removal-in-cincinnati": _2,
+  "rat-inspection-in-cincinnati": _3,
+  "rodent-exclusion-in-cincinnati": _4,
+  "attic-rat-cleanup-in-cincinnati": _5,
+  "dead-rat-removal-in-cincinnati": _6,
+  "commercial-rat-control-in-cincinnati": _7,
+  "mouse-control-in-cincinnati": _8,
+};
